bf82a9fde3df83c84bb328af99e718952a1a8889
H-Soba Vincent <hsobav@gmail.com>
2020-12-24 13:40:06 -0500

    first attempt.

--- InsertionSort.cs
+++ InsertionSort.cs
@@ -10,6 +10,30 @@ namespace InsertionSort
     // an integer array in ascending order
     public class InsertionSort
     {
+        private static bool More(int value1, int value2)
+        {
+            return value1 > value2;     // returns true if value1 is more than value2
+        }
+
+        public static void Sort(int[] arr)
+        {
+            int size = arr.Length;
+
+            int i, j, temp;
+
+            for (i = 0; i < size - 1; i++)
+            {
+                for (j = 0; j < size - 1; j++)
+                {
+                    while (More(arr[j], arr[j + 1]))
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace InsertionSort
 {
@@ -10,7 +11,27 @@ namespace InsertionSort
     {
         static void Main(string[] args)
         {
+            Stopwatch myTimer = new Stopwatch();
+
             int[] exampleArray = new int[] { 342, 242, 235, 64, 474, 3, 100, 2 };
+
+            Console.Write("Before sorting: ");
+            foreach (int element in exampleArray)
+            {
+                Console.Write(element + " ");
+            }
+
+            myTimer.Start();
+            InsertionSort.Sort(exampleArray);
+            myTimer.Stop();
+
+            Console.WriteLine();
+            Console.Write("After sorting: ");
+            foreach (int element in exampleArray)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine("\nTime taken: " + myTimer.ElapsedTicks);
         }
     }
 }
